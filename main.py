8267d52cc90917128e4a562673d6f4d6dc275aaa
JAadarsh <aadarshjoshijii@gmail.com>
2026-06-20 22:36:36 -0700

    more fixes to main.py

--- main.py
+++ main.py
@@ -49,6 +49,7 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game(name="/setmessage"))
     print(f'Logged in as {bot.user.name} (ID: {bot.user.id})')
 
     bot.db = Database(supabase_url, supabase_key)
